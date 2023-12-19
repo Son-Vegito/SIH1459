a949f539c4d9e7f31b7fa1b3a4d98edf9c65a237
Vivek9411 <Vivkumar941121@gmail.com>
2023-12-19 23:24:45 +0530

    added primary key generation

--- SIH1459/website/models.py
+++ SIH1459/website/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 
+
+
+
 # Create your models here.
 class State(models.Model):
     name = models.CharField('State Name',max_length=50)
@@ -64,10 +67,18 @@ class Scheme(models.Model):
         return self.name
 
 
+def generate_pk(admission_year, course, college, serial_no):
+    # Your custom logic here
+    y=str(College.objects.get(name=college).code)+str(admission_year)[-2:]+str(Course.objects.get(name=course).code)+str(serial_no)
+    print(y)
+    return y
+
+
 class Student(models.Model):
     first_name = models.CharField('First Name',max_length=100)
     last_name = models.CharField('Last Name',max_length=100)
-    uid = models.CharField('UID', primary_key=True, max_length=15, validators=[MinLengthValidator(13), MaxLengthValidator(13)])
+    serial_no = models.CharField('Serial No', max_length=3, validators=[MinLengthValidator(3), MaxLengthValidator(3)])
+    uid = models.CharField('UID', primary_key=True, max_length=15, validators=[MinLengthValidator(13), MaxLengthValidator(13)], )
     admission_year = models.CharField('Admission Year', max_length=4, validators=[MinLengthValidator(4), MaxLengthValidator(4)])
     date_of_birth = models.DateField('Date of Birth', null=False, blank=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -81,6 +92,10 @@ class Student(models.Model):
             models.Index(fields=['aadhar_id']),
             models.Index(fields=['first_name', 'last_name']),
         ]
+
+    def save(self, *args, **kwargs):
+        self.uid = generate_pk(self.admission_year, self.course, self.college, self.serial_no)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
